764774d7637a097e780097d85a4428554f901ff3
Rennan Victor <rennanparadeda@gmail.com>
2026-04-22 22:39:26 -0300

    resolução ex060

---
+++ ex060.py
@@ -0,0 +1,14 @@
+'''Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
+
+5! = 5 x 4 x 3 x 2 x 1 = 120'''
+
+
+numero = int(input('Digite um numero para calcular seu fatorial: '))
+contador = numero
+fatorial = 1
+while contador > 0:
+    print('{}'.format(contador), end=' ')
+    print('x' if contador > 1 else '=', end=' ')
+    fatorial = fatorial * contador
+    contador = contador - 1
+print(fatorial)
