fcc6f345fad6c843c15b202e901ae64285874728
Rennan Victor <rennanparadeda@gmail.com>
2026-05-03 00:45:19 -0300

    comentário
    adicionado comentário no código

--- ex063.py
+++ ex063.py
@@ -5,16 +5,26 @@
 print('-=' * 20)
 print('Sequancia de Fibonacci')
 print('-=' * 20)
+#inicialização do primeiro termo, segundo termo e terceiro termo
 primeirotermo = 0
 segundotermo = 1
 terceirotermo = primeirotermo + segundotermo
+
+# Quantidade de termos a ser escolhidos pra ser mostrado
 quantidade = int(input('Quantos termos você quer mostrar?: '))
+
+# Mostra o primeiro e o segundo termo lado a lado
 print('{} -> '.format(primeirotermo), end='')
 print('{} -> '.format(segundotermo), end='')
+
+# inicia o contador a partir do segundo termo e somando os dois primeiros 
 contador = 2
 soma = terceirotermo
-while contador < quantidade:
+while contador < quantidade: # enquanto a quantidade de termos for menor que a quantidade escolhida...
     print('{} -> '.format(soma), end='')
+
+    ''' Bloco de comando que faz com que o primeiro termo termo receba o segundo, o segundo receba a soma e
+    o terceiro some os novos valores'''
     primeirotermo = segundotermo
     segundotermo = terceirotermo
     terceirotermo = primeirotermo + segundotermo
