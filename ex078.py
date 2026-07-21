47230253593ebb54a7c4c6b8fe3f7bd50e78bf47
Rennan Victor <rennanparadeda@gmail.com>
2026-07-20 21:16:53 -0400

    resolução ex078

---
+++ ex078.py
@@ -0,0 +1,25 @@
+'''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.'''
+
+valores = []
+for cont in range(0, 5):
+    valores.append(int(input('Digite um valor para a posição {}: '.format(cont))))
+
+for cont, valor in enumerate(valores):
+    if cont == 0:
+        maiorValor = valor
+        menorValor = valor
+    else:
+        if valor > maiorValor:
+            maiorValor = valor
+        if valor < menorValor:
+            menorValor = valor
+
+print(f'Você digitou os valores {valores}')
+print(f'O maior valor digitado foi {maiorValor} nas posições ')
+for c in range(0, 5):
+    if valores[c] == maiorValor:
+        print(f'{c}...')
+print(f'O menor valor digitado foi {menorValor} nas posições ')
+for c in range(0, 5):
+    if valores[c] == menorValor:
+        print(f'{c}...')
