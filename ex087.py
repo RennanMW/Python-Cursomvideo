83d4fca1b41705e4718d7c8c86076abde3fb3678
Rennan Victor <rennanparadeda@gmail.com>
2026-09-16 22:41:13 -0400

    Ex087
    Leia uma matriz 3x3 e some os números pares e some os valores da terceira coluna.
    Ainda não finalizado

---
+++ ex087.py
@@ -0,0 +1,30 @@
+'''Aprimore o desafio anterior, mostrando no final: 
+
+A) A soma de todos os valores pares digitados. 
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.'''
+matriz = [[], [], []]
+soma_par = 0
+soma_terceira_coluna = 0
+
+# Loop que faz a leitura das posições da matriz, linha e coluna.
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        numero = int(input(f'Digite um valor para [{linha},{coluna}]: '))
+        matriz[linha].append(numero)
+
+        if numero % 2 == 0: # Verifica se o valor digitado é par.
+            soma_par = soma_par + numero
+
+# Loop que pega o 3° valor de cada linha, ou seja, a 3° coluna da matriz e soma seus números.
+for linha in range(0, 3):
+    soma_terceira_coluna = soma_terceira_coluna + matriz[linha][2]
+
+print('-=' * 30)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print()
+print('-=' * 30)
+print(f'A soma dos valores pares é {soma_par}.')
+print(f'A soma dos valores da terceira coluna é {soma_terceira_coluna}')
